8ea0bdefe3c22d236dc22d5c0f4cfc3cb62802c2
MARATTHEGOAT <92694308+vasilievmi@users.noreply.github.com>
2021-10-29 20:08:20 +0400

    Delete TASK6.py

--- lab3.2/TASK6.py
+++
@@ -1,9 +0,0 @@
-
-
-price = int(input("Цена за 1 кг = "))
-print('Цена 1 кг конфет: ', price)
-print()
-for i in range(0, 10):
-    x = 0.1 + 0.1 * i
-    print('Стоимость {0:.1f} кг: {1:.2f}'.format(x, x * price))
-print()
